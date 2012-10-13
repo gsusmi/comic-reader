41d732116e15f0d0e674ad484000552dbff5c998
gsusmi <gsusmi@gmail.com>
2012-10-13 18:22:19 -0400

    log to rails logger damn  you

--- config/deploy.rb
+++ config/deploy.rb
@@ -95,8 +95,8 @@ end
 
 after "deploy:stop",    "delayed_job:stop"
 after "deploy:start",   "delayed_job:start"
-after "deploy:restart",   "deploy:migrate"
-after "deploy:restart",   "deploy:seed"
+after "deploy:restart", "deploy:migrate"
+after "deploy:restart", "deploy:seed"
 after "deploy:restart", "delayed_job:restart"
 
 # Restart Passenger
