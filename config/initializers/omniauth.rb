be6ad1a384bc744554df28b100b1c11f2aef9562
Darshan Shaligram <scintilla@gmail.com>
2012-10-14 13:08:04 -0400

    Remove tmp-file storage provider, switch to in-memory provider.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,5 @@ require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :open_id, store: OpenID::Store::Filesystem.new('/tmp'),
-           :identifier => 'https://www.google.com/accounts/o8/id'
+  provider :open_id, :identifier => 'https://www.google.com/accounts/o8/id'
 end
