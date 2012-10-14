41fce8317ac8fe6da3074ee009a0bd80195b8761
Darshan Shaligram <scintilla@gmail.com>
2012-10-14 13:53:59 -0400

    Save feed settings.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -9,4 +9,16 @@ class SettingsController < ApplicationController
     @available_feeds = Feed.available_feeds
     @subscribed_feeds = Set.new(self.user_or_anon.feeds.map(&:id))
   end
+
+  def update
+    logger.info("params: #{params.inspect}")
+    user = self.current_user
+    user.feeds =
+      params.keys.find_all { |key| key =~ /^feed_\d+/ }.map { |key|
+        Feed.find(params[key].to_i)
+      }
+    user.save!
+    flash[:message] = 'Saved your subscriptions'
+    redirect_to(action: 'index')
+  end
 end
