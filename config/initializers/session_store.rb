9fdd0d32a4f56ce328558261fe6400b7d2963b9d
Darshan Shaligram <scintilla@gmail.com>
2012-10-14 13:00:16 -0400

    Go back to cookie store, we don't need session tables.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Rcomix::Application.config.session_store :cookie_store, key: '_rcomix_session'
+Rcomix::Application.config.session_store :cookie_store, key: '_rcomix_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-Rcomix::Application.config.session_store :active_record_store
+#Rcomix::Application.config.session_store :active_record_store
