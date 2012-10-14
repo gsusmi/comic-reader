0b2eed678c8485fa3437cce0f69f537fa782a597
Darshan Shaligram <scintilla@gmail.com>
2012-10-14 09:57:52 -0400

    Run the feed sync on launch only in production deploy.

--- config/initializers/schedule_feed_sync.rb
+++ config/initializers/schedule_feed_sync.rb
@@ -1,4 +1,4 @@
-if File.basename($0) != 'rake'
+if File.basename($0) != 'rake' && Rails.env.production?
   Rails.logger.info("[#{Time.now}] Starting delayed task")
   Sync::FeedSyncTask.new.perform
 end
